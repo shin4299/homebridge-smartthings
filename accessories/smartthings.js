ce0d0f73eb06ff16e9c2e879c1b86543b4fdeca4
shin4299 <shin4299@gmail.com>
2017-11-27 02:45:37 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -222,10 +222,20 @@ function SmartThingsAccessory(platform, device) {
         
     }
     if (device.capabilities["Switch"] !== undefined && this.deviceGroup == "unknown") {
-	    	        if (device.commands.Outlet) {
+	    	    if (device.commands.Outlet) {
             this.deviceGroup = "outlet"
-            
             thisCharacteristic = this.getaddService(Service.Outlet).getCharacteristic(Characteristic.On)
+            thisCharacteristic.on('get', function(callback) { callback(null, that.device.attributes.switch == "on"); })
+            thisCharacteristic.on('set', function(value, callback) {
+                    if (value)
+                        that.platform.api.runCommand(callback, that.deviceid, "on");
+                    else
+                        that.platform.api.runCommand(callback, that.deviceid, "off"); });
+		        that.platform.addAttributeUsage("switch", this.deviceid, thisCharacteristic);
+}
+	    	    	    if (device.commands.Faucet) {
+            this.deviceGroup = "faucet"
+            thisCharacteristic = this.getaddService(Service.Faucet).getCharacteristic(Characteristic.On)
             thisCharacteristic.on('get', function(callback) { callback(null, that.device.attributes.switch == "on"); })
             thisCharacteristic.on('set', function(value, callback) {
                     if (value)
