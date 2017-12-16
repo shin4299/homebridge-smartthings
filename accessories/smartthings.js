35db932e65769e47d4b3563aef2f1f1f0d745e32
shin4299 <shin4299@gmail.com>
2017-12-16 17:20:02 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -307,19 +307,12 @@ function SmartThingsAccessory(platform, device) {
     }
 	
     if (device.capabilities["Illuminance Measurement"] !== undefined) {
-	 if (device.commands.Refresh)    {
-         this.deviceGroup = "sensor";
+	 if (this.deviceGroup == 'unknown') this.deviceGroup = "sensor";
         thisCharacteristic = this.getaddService(Service.LightSensor).getCharacteristic(Characteristic.CurrentAmbientLightLevel)
         thisCharacteristic.on('get', function(callback) { callback(null, Math.round(that.device.attributes.Light)); });
 		that.platform.addAttributeUsage("Light", this.deviceid, thisCharacteristic);
     	}
-        else {
-         this.deviceGroup = "sensor";
-        thisCharacteristic = this.getaddService(Service.LightSensor).getCharacteristic(Characteristic.CurrentAmbientLightLevel)
-        thisCharacteristic.on('get', function(callback) { callback(null, Math.round(that.device.attributes.illuminance)); });
-		that.platform.addAttributeUsage("illuminance", this.deviceid, thisCharacteristic);
-    	}
-    }
+	
     if (device.capabilities["Temperature Measurement"] !== undefined) {
         if (this.deviceGroup == 'unknown') this.deviceGroup = "sensor";
         thisCharacteristic = this.getaddService(Service.TemperatureSensor).getCharacteristic(Characteristic.CurrentTemperature)
