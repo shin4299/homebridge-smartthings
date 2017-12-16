f6e8d27b8a92827863dacfe6413d02b471e788c0
shin4299 <shin4299@gmail.com>
2017-12-16 17:11:50 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -308,13 +308,13 @@ function SmartThingsAccessory(platform, device) {
 	
     if (device.capabilities["Illuminance Measurement"] !== undefined) {
 	 if (device.commands.Refresh)    {
-        if (this.deviceGroup == 'unknown') this.deviceGroup = "sensor";
+         this.deviceGroup = "sensor";
         thisCharacteristic = this.getaddService(Service.LightSensor).getCharacteristic(Characteristic.CurrentAmbientLightLevel)
         thisCharacteristic.on('get', function(callback) { callback(null, Math.round(that.device.attributes.Light)); });
 		that.platform.addAttributeUsage("Light", this.deviceid, thisCharacteristic);
     	}
         else {
-        if (this.deviceGroup == 'unknown') this.deviceGroup = "sensor";
+         this.deviceGroup = "sensor";
         thisCharacteristic = this.getaddService(Service.LightSensor).getCharacteristic(Characteristic.CurrentAmbientLightLevel)
         thisCharacteristic.on('get', function(callback) { callback(null, Math.round(that.device.attributes.illuminance)); });
 		that.platform.addAttributeUsage("illuminance", this.deviceid, thisCharacteristic);
