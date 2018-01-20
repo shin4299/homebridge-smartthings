51253571f7df87049e5df72957cfdc33b29f2f8d
shin4299 <shin4299@gmail.com>
2018-01-21 03:14:40 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -473,20 +473,20 @@ function SmartThingsAccessory(platform, device) {
 		
 		
 
-	var TotalPowerConsumption = function() {
+	this.TotalPowerConsumption = function() {
 		Characteristic.call(this, 'Total Consumption', 'E863F10C-079E-48FF-8F27-9C2605A29F52');
 		this.setProps({
 			format: Characteristic.Formats.FLOAT, // Deviation from Eve Energy observed type
 			unit: 'kilowatthours',
 			maxValue: 1000000000,
 			minValue: 0,
-			minStep: 0.001,
+			minStep: 0.1,
 			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
 		});
 		this.value = this.getDefaultValue();
 	};
 
-	inherits(TotalPowerConsumption, Characteristic);
+	inherits(this.TotalPowerConsumption, Characteristic);
 		
 		
         thisCharacteristic = this.getaddService(Service.Outlet).getCharacteristic(Characteristic.TotalPowerConsumption)
