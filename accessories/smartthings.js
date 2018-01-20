8c9fc6df2bb81e14b2100b78879beaaca3c493d3
shin4299 <shin4299@gmail.com>
2018-01-20 22:33:50 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -471,6 +471,13 @@ function SmartThingsAccessory(platform, device) {
         if(device.commands.energy) {
 	 if (this.deviceGroup == 'unknown') this.deviceGroup = "sensor";
         thisCharacteristic = this.getaddService(Service.LightSensor).getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+        thisCharacteristic = this.setProps({
+            unit: "KWh",
+            maxValue: 4294967295,
+            minValue: 0,
+            minStep: 0.1,
+        });
+		
         thisCharacteristic.on('get', function(callback) { callback(null, Math.round(that.device.attributes.energy)); });
                 that.platform.addAttributeUsage("energy", this.deviceid, thisCharacteristic);
         }
