bc77c4ac93972a4ca220343ba83b99dd9db10ee3
shin4299 <shin4299@gmail.com>
2018-01-21 06:45:45 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -12,7 +12,7 @@ module.exports = function(oAccessory, oService, oCharacteristic, ouuid) {
         Service = oService;
         Characteristic = oCharacteristic;
         EnergyCharacteristics = require('../lib/customCharacteristics').EnergyCharacteristics(Characteristic)
-        TotalPowerConsumption = require('../lib/TotalPowerConsumption').TotalPowerConsumption
+//        TotalPowerConsumption = require('../lib/TotalPowerConsumption').TotalPowerConsumption
 
         uuid = ouuid;
 
@@ -474,13 +474,13 @@ function SmartThingsAccessory(platform, device) {
         if(device.commands.energy) {
 		
 	 if (this.deviceGroup == 'unknown') this.deviceGroup = "Energy Meter";
-        thisCharacteristic = this.getaddService(Service.HumiditySensor).getCharacteristic(Characteristic.TotalPowerConsumption)
+        thisCharacteristic = this.getaddService(Service.HumiditySensor).addCharacteristic(EnergyCharacteristics.TotalPowerConsumption)
         thisCharacteristic.on('get', function(callback) { callback(null, Math.round(that.device.attributes.energy)); })
                 that.platform.addAttributeUsage("energy", this.deviceid, thisCharacteristic);
         }
 	    else {
         this.deviceGroup = 'EnergyMeter';
-        thisCharacteristic = this.getaddService(Service.Outlet).addCharacteristic(Characteristic.TotalPowerConsumption)
+        thisCharacteristic = this.getaddService(Service.Outlet).addCharacteristic(EnergyCharacteristics.TotalPowerConsumption)
         thisCharacteristic.on('get', function(callback) { callback(null, Math.round(that.device.attributes.energy)); });
 		that.platform.addAttributeUsage("energy", this.deviceid, thisCharacteristic);
     	}
