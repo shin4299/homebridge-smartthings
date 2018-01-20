eb7a2f9709bc532d423b244a9bc3433ebed0052f
shin4299 <shin4299@gmail.com>
2018-01-21 04:43:37 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -483,15 +483,14 @@ function SmartThingsAccessory(platform, device) {
 		
 	 if (this.deviceGroup == 'unknown') this.deviceGroup = "Energy Meter";
 				
-        thisCharacteristic = this.getaddService(Service.Outlet).getCharacteristic(Characteristic.call(this, 'Total Consumption', 'E863F10C-079E-48FF-8F27-9C2605A29F52');
-        this.setProps({
+        thisCharacteristic = this.getaddService(Service.Outlet).getCharacteristic(Characteristic.call(this, 'Total Consumption', 'E863F10C-079E-48FF-8F27-9C2605A29F52')).setProps({
             format: Characteristic.Formats.FLOAT, // Deviation from Eve Energy observed type
             unit: 'KWh',
             maxValue: 1000000000,
             minValue: 0,
             minStep: 0.1,
             perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-        }))
+        })
         thisCharacteristic.on('get', function(callback) { callback(null, Math.round(that.device.attributes.energy)); })
                 that.platform.addAttributeUsage("energy", this.deviceid, thisCharacteristic);
         }
