b0014ad43342c9fbbd4e7e37c09aca5d902fe266
shin4299 <shin4299@gmail.com>
2018-01-21 20:17:00 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -12,7 +12,7 @@ module.exports = function(oAccessory, oService, oCharacteristic, ouuid) {
         Service = oService;
         Characteristic = oCharacteristic;
         EnergyCharacteristics = require('../lib/customCharacteristics').EnergyCharacteristics(Characteristic)
-        TotalPowerConsumption = require('../lib/TotalPowerConsumption')
+        TotalPowerConsumption = require('../lib/TotalPowerConsumption').Energy(Characteristic)
 
         uuid = ouuid;
 
