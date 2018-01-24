27d8057319b93efd92081f1f7d557dd498d080ec
shin4299 <shin4299@gmail.com>
2018-01-24 20:11:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var smartthings = require('./lib/smartthingsapi');
 var http = require('http')
 var os = require('os');
 
-var Service, Characteristic, Accessory, uuid, EnergyCharacteristics, CurrentPowerConsumption;
+var Service, Characteristic, Accessory, uuid, EnergyCharacteristics;
 
 var SmartThingsAccessory;
 
