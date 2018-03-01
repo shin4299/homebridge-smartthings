8698da1afd0eadfa4d3bb8a2ed4f4fb22020985f
shin4299 <shin4299@gmail.com>
2018-03-01 11:08:36 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -110,6 +110,40 @@ function SmartThingsAccessory(platform, device) {
             	    	that.platform.api.runCommand(callback, that.deviceid, "setLevel", {value1: value }); });
 			that.platform.addAttributeUsage("level", this.deviceid, thisCharacteristic);
         
+        } else if (device.commands.kukufan) {
+            //This is a Ceiling Fan
+            this.deviceGroup = "fans"
+            
+            thisCharacteristic = this.getaddService(Service.Fan).getCharacteristic(Characteristic.On)
+            thisCharacteristic.on('get', function(callback) { callback(null, that.device.attributes.switch == "on"); })
+            thisCharacteristic.on('set', function(value, callback) {
+                    if (value)
+                        that.platform.api.runCommand(callback, that.deviceid, "on");
+                    else
+                        that.platform.api.runCommand(callback, that.deviceid, "off"); });
+		        that.platform.addAttributeUsage("switch", this.deviceid, thisCharacteristic);
+
+	        thisCharacteristic = this.getaddService(Service.Fan).getCharacteristic(Characteristic.RotationSpeed)
+            thisCharacteristic.on('get', function(callback) { callback(null, parseInt(that.device.attributes.level)); });
+            thisCharacteristic.on('set', function(value, callback) { 
+            	    if (value > 0)
+            	    	that.platform.api.runCommand(callback, that.deviceid, "setLevel", {value1: value }); });
+			that.platform.addAttributeUsage("level", this.deviceid, thisCharacteristic);
+
+            thisCharacteristic = this.getaddService(Service.Fan).getCharacteristic(Characteristic.SwingMode)
+        thisCharacteristic.on('get', function(callback) {
+                if (that.device.attributes.swingMode == 'on')
+                    callback(null, Characteristic.SwingMode.SWING_ENABLED);
+                else
+                    callback(null, Characteristic.SwingMode.SWING_DISABLED);
+            });
+            thisCharacteristic.on('set', function(value, callback) {
+                    if (value)
+                        that.platform.api.runCommand(callback, that.deviceid, "swingon");
+                    else
+                        that.platform.api.runCommand(callback, that.deviceid, "swingoff"); });
+		        that.platform.addAttributeUsage("swingMode", this.deviceid, thisCharacteristic);		
+		
         } else {
             this.deviceGroup = "lights";
             thisCharacteristic = this.getaddService(Service.Lightbulb).getCharacteristic(Characteristic.On)
