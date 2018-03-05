5a78f7b666f4e3a8141e17ea278115c0265fcef7
shin4299 <shin4299@gmail.com>
2018-03-06 03:45:08 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -111,8 +111,10 @@ function SmartThingsAccessory(platform, device) {
 			that.platform.addAttributeUsage("level", this.deviceid, thisCharacteristic);
 
             thisCharacteristic = this.getaddService(Service.Fan).getCharacteristic(Characteristic.SwingMode)
-            thisCharacteristic.on('get', function(callback) {
-/*		 switch (that.device.attributes.swingMode) {
+            thisCharacteristic.on('get', function(callback) { callback(null, that.device.attributes.swingMode === "on" ? Characteristic.SwingMode.SWING_ENABLED : Characteristic.SwingMode.SWING_DISABLED); })
+		
+/*            thisCharacteristic.on('get', function(callback) {
+		 switch (that.device.attributes.swingMode) {
                     case 'swingon':
                         callback(null, Characteristic.SwingMode.SWING_ENABLED);
                         break;
@@ -123,13 +125,14 @@ function SmartThingsAccessory(platform, device) {
                         callback(null, Characteristic.SwingMode.SWING_DISABLED);
                         break;
                 }
-*/		 
+		 
 
                 if (that.device.attributes.swingMode == "on")
                     callback(null, Characteristic.SwingMode.SWING_ENABLED);
                 else
                     callback(null, Characteristic.SwingMode.SWING_DISABLED);
-            });
+		    
+            });*/
             thisCharacteristic.on('set', function(value, callback) {
                 if (value == Characteristic.SwingMode.SWING_ENABLED) {
                     that.platform.api.runCommand(callback, that.deviceid, "swingon");
