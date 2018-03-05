60056eb3caae9264b3539dcd026ebdf239f38e31
shin4299 <shin4299@gmail.com>
2018-03-06 00:40:34 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -111,12 +111,13 @@ function SmartThingsAccessory(platform, device) {
 			that.platform.addAttributeUsage("level", this.deviceid, thisCharacteristic);
 
             thisCharacteristic = this.getaddService(Service.Fan).getCharacteristic(Characteristic.SwingMode)
-        thisCharacteristic.on('get', function(callback) {
-                if (that.device.attributes.swingMode == "on")
-                    callback(null, Characteristic.SwingMode.SWING_ENABLED);
-                else
-                    callback(null, Characteristic.SwingMode.SWING_DISABLED);
-            });
+            thisCharacteristic.on('get', function(callback) { callback(null, that.device.attributes.swingMode == "on"); })
+//            thisCharacteristic.on('get', function(callback) {
+//                if (that.device.attributes.swingMode == "on")
+//                    callback(null, Characteristic.SwingMode.SWING_ENABLED);
+//                else
+//                    callback(null, Characteristic.SwingMode.SWING_DISABLED);
+//            });
             thisCharacteristic.on('set', function(value, callback) {
                 if (value == Characteristic.SwingMode.SWING_ENABLED) {
                     that.platform.api.runCommand(callback, that.deviceid, "swingon");
