2b65c49e3ce5e8736bca9851720d4765ee198cca
shin4299 <shin4299@gmail.com>
2018-03-06 05:38:53 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -112,10 +112,10 @@ function SmartThingsAccessory(platform, device) {
 
             thisCharacteristic = this.getaddService(Service.Fan).getCharacteristic(Characteristic.SwingMode)		
             thisCharacteristic.on('get', function(callback) {
-		if (that.device.attributes.swingMode == 'ON')
-                    callback(null, Characteristic.SwingMode.SWING_DISABLED);
-                else
+		if (that.device.attributes.swingMode == "on")
                     callback(null, Characteristic.SwingMode.SWING_ENABLED);
+                else
+                    callback(null, Characteristic.SwingMode.SWING_DISABLED);
             });
 		    
             thisCharacteristic.on('set', function(value, callback) {
