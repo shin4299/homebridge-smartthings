332ef0ccde22f475120f30aa4078a5fc9066a213
shin4299 <shin4299@gmail.com>
2018-03-10 00:30:20 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -111,20 +111,20 @@ function SmartThingsAccessory(platform, device) {
 			that.platform.addAttributeUsage("level", this.deviceid, thisCharacteristic);
 
             thisCharacteristic = this.getaddService(Service.Fan).getCharacteristic(Characteristic.SwingMode)		
-/*            thisCharacteristic.on('get', function(callback) { callback(null, that.device.attributes.swingMode == "on"); })		
+//            thisCharacteristic.on('get', function(callback) { callback(null, that.device.attributes.swingMode == "on"); })		
             thisCharacteristic.on('get', function(callback) {
 		if (that.device.attributes.swingMode == 'off' )
                     callback(null, Characteristic.SwingMode.SWING_DISABLED);
                 else if (that.device.attributes.swingMode == 'on' )
                     callback(null, Characteristic.SwingMode.SWING_ENABLED);
             });		    
-*/		
-            thisCharacteristic.on('set', function(value, callback) {
+		
+/*            thisCharacteristic.on('set', function(value, callback) {
                 if (value == Characteristic.SwingMode.SWING_ENABLED) {
                     that.platform.api.runCommand(callback, that.deviceid, "swingMode");
                 } else if (value == Characteristic.SwingMode.SWING_DISABLED) {
                     that.platform.api.runCommand(callback, that.deviceid, "swingMode");
-                } });
+                } }); */
 		 that.platform.addAttributeUsage("swingMode", this.deviceid, thisCharacteristic);	
 
 		
