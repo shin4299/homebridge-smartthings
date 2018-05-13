d2fd14f1a57f39d0e97c76d9a3150c4a638ddfab
shin4299 <shin4299@gmail.com>
2018-05-14 00:33:08 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -373,13 +373,13 @@ else if (device.capabilities["Valve"] !== undefined){
                 else
                     that.platform.api.runCommand(callback, that.deviceid, "off");
             });
-            that.platform.addAttributeUsage('active', this.deviceid, thisCharacteristic);	
+            that.platform.addAttributeUsage('switch', this.deviceid, thisCharacteristic);	
 	
 	    thisCharacteristic = this.getaddService(Service.Valve).getCharacteristic(Characteristic.InUse);
             thisCharacteristic.on('get', function(callback) {
                 callback(null, that.device.attributes.switch == "on");
             });
-            that.platform.addAttributeUsage('inUse', this.deviceid, thisCharacteristic);
+            that.platform.addAttributeUsage('switch', this.deviceid, thisCharacteristic);
 
 	
 }
