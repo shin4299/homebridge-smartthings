ef6901221b8b2c7522e81e8d45b042cae2615196
shin4299 <shin4299@gmail.com>
2018-09-14 00:45:03 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -250,6 +250,7 @@ function SmartThingsAccessory(platform, device) {
     thisCharacteristic = this.getaddService(Service.HumidifierDehumidifier).getCharacteristic(Characteristic.TargetHumidifierDehumidifierState).setProps({ validValues: [1] });
     that.platform.addAttributeUsage("switch", this.deviceid, thisCharacteristic);
 
+    /*            
     thisCharacteristic = this.getaddService(Service.HumidifierDehumidifier).getCharacteristic(Characteristic.TargetFanState)
     thisCharacteristic.on('get', function(callback) {
             if (that.device.attributes.mode == "auto")
@@ -267,7 +268,6 @@ function SmartThingsAccessory(platform, device) {
     });		    
     that.platform.addAttributeUsage("mode", this.deviceid, thisCharacteristic);
     
-    /*
         
         thisCharacteristic = this.getaddService(Service.HumidifierDehumidifier).getCharacteristic(Characteristic.TargetHumidifierDehumidifierState)
         thisCharacteristic.on('get', function (callback) {
