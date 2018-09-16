8b91b714a82b5f572619d4498d9671dac9e64b45
shin4299 <shin4299@gmail.com>
2018-09-17 02:29:48 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -792,19 +792,43 @@ if (device.attributes['securityStatus'] !== undefined) {
 
 if (device.capabilities["Button"] !== undefined) {
     this.deviceGroup = "button";
+
+    thisCharacteristic = this.getaddService(Service.StatelessProgrammableSwitch).getCharacteristic(Characteristic.ProgrammableSwitchEvent)
+//        if (that.device.attributes.button == 'pushed')
+            thisCharacteristic.setValue(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS, that.device.attributes.button == 'pushed');
+//        else if (that.device.attributes.button == 'double')
+            thisCharacteristic.setValue(Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS, that.device.attributes.button == 'double');
+//        else if (that.device.attributes.button == 'held')
+            thisCharacteristic.setValue(Characteristic.ProgrammableSwitchEvent.LONG_PRESS, that.device.attributes.button == 'held');
+    that.platform.addAttributeUsage("button", this.deviceid, thisCharacteristic);
+/*
+
+            thisCharacteristic = this.getaddService(Service.Valve).getCharacteristic(Characteristic.ValveType);
+            thisCharacteristic.on('get', function (callback) {
+                switch (that.device.attributes.valvetype) {
+                    case 'Faucet':
+                        callback(null, Characteristic.ValveType.WATER_FAUCET);
+                        break;
+                    case 'ShowerHead':
+                        callback(null, Characteristic.ValveType.SHOWER_HEAD);
+                        break;
+                    case 'Sprinkler':
+                        callback(null, Characteristic.ValveType.IRRIGATION);
+                        break;
+                    case 'GenericValve':
+                    default:
+                        callback(null, Characteristic.ValveType.GENERIC_VALVE);
+                        break;
+                }
+            });
+            that.platform.addAttributeUsage('valvetype', this.deviceid, thisCharacteristic);
+
+
 	thisCharacteristic = this.getaddService(Service.StatelessProgrammableSwitch).setCharacteristic(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS, that.device.attributes.button == 'pushed')
 	thisCharacteristic = this.getaddService(Service.StatelessProgrammableSwitch).setCharacteristic(Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS, that.device.attributes.button == 'double')
 	thisCharacteristic = this.getaddService(Service.StatelessProgrammableSwitch).setCharacteristic(Characteristic.ProgrammableSwitchEvent.LONG_PRESS, that.device.attributes.button == 'held')
     	that.platform.addAttributeUsage("button", this.deviceid, thisCharacteristic);
 
-/*    thisCharacteristic = this.getaddService(Service.StatelessProgrammableSwitch).getCharacteristic(Characteristic.ProgrammableSwitchEvent)
-        if (that.device.attributes.button == 'pushed')
-            thisCharacteristic.setValue(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS);
-        else if (that.device.attributes.button == 'double')
-            thisCharacteristic.setValue(Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS);
-        else if (that.device.attributes.button == 'held')
-            thisCharacteristic.setValue(Characteristic.ProgrammableSwitchEvent.LONG_PRESS);
-    that.platform.addAttributeUsage("button", this.deviceid, thisCharacteristic);
 
     if (gesture == '1') {
         this.switch1Service
