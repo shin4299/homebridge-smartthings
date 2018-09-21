519dfe1f22986a323aaa729449505926fbfe4a7a
shin4299 <shin4299@gmail.com>
2018-09-21 16:15:47 +0900

    Update smartthings.js

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -1501,6 +1501,116 @@ if (device.capabilities["Three Axis"] !== undefined) {
 }
 
 if (device.capabilities["Thermostat"] !== undefined) {
+if (device.commands.cooler) {
+    this.deviceGroup = "thermostats";
+
+    thisCharacteristic = this.getaddService(Service.HeaterCooler).getCharacteristic(Characteristic.Active)
+    thisCharacteristic.on('get', function (callback) {
+        if (that.device.attributes.switch == "on")
+            callback(null, Characteristic.Active.ACTIVE);
+        else
+            callback(null, Characteristic.Active.INACTIVE);
+    });
+    thisCharacteristic.on('set', function (value, callback) {
+        if (value)
+            that.platform.api.runCommand(callback, that.deviceid, "on");
+        else
+            that.platform.api.runCommand(callback, that.deviceid, "off");
+    });
+    that.platform.addAttributeUsage("switch", this.deviceid, thisCharacteristic);
+
+    thisCharacteristic = this.getaddService(Service.HeaterCooler).getCharacteristic(Characteristic.CurrentHeaterCoolerState).setProps({ validValues: [0, 1, 3] });
+    thisCharacteristic.on('get', function (callback) {
+        if (that.device.attributes.power <= 2)
+            callback(null, Characteristic.CurrentHeaterCoolerState.INACTIVE);
+        else if (that.device.attributes.power <= 50)
+            callback(null, Characteristic.CurrentHeaterCoolerState.IDLE);
+        else
+            callback(null, Characteristic.CurrentHeaterCoolerState.COOLING);
+    });
+    that.platform.addAttributeUsage("power", this.deviceid, thisCharacteristic);
+    /*
+        thisCharacteristic = this.getaddService(Service.HeaterCooler).getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+        thisCharacteristic.on('get', function (callback) {
+            switch (that.device.attributes.thermostatOperatingState) {
+                case "pending cool":
+                case "cooling":
+                    callback(null, Characteristic.CurrentHeatingCoolingState.COOL);
+                    break;
+                case "pending heat":
+                case "heating":
+                    callback(null, Characteristic.CurrentHeatingCoolingState.HEAT);
+                    break;
+                default: //The above list should be inclusive, but we need to return something if they change stuff.
+                    //TODO: Double check if Smartthings can send "auto" as operatingstate. I don't think it can.
+                    callback(null, Characteristic.CurrentHeatingCoolingState.OFF);
+                    break;
+            }
+        });
+        that.platform.addAttributeUsage("thermostatOperatingState", this.deviceid, thisCharacteristic);
+        */
+    //Handle the Target State
+    thisCharacteristic = this.getaddService(Service.HeaterCooler).getCharacteristic(Characteristic.TargetHeaterCoolerState).setProps({ validValues: [2] });
+    that.platform.addAttributeUsage("switch", this.deviceid, thisCharacteristic);
+    /*    thisCharacteristic.on('get', function (callback) {
+            switch (that.device.attributes.thermostatMode) {
+                case "cool":
+                    callback(null, Characteristic.TargetHeatingCoolingState.COOL);
+                    break;
+                case "emergency heat":
+                case "heat":
+                    callback(null, Characteristic.TargetHeatingCoolingState.HEAT);
+                    break;
+                case "auto":
+                    callback(null, Characteristic.TargetHeatingCoolingState.AUTO);
+                    break;
+                default: //The above list should be inclusive, but we need to return something if they change stuff.
+                    callback(null, Characteristic.TargetHeatingCoolingState.OFF);
+                    break;
+            }
+        })
+        thisCharacteristic.on('set', function (value, callback) {
+            switch (value) {
+                case Characteristic.TargetHeatingCoolingState.COOL:
+                    that.platform.api.runCommand(callback, that.deviceid, "cool");
+                    that.device.attributes.thermostatMode = 'cool';
+                    break;
+                case Characteristic.TargetHeatingCoolingState.HEAT:
+                    that.platform.api.runCommand(callback, that.deviceid, "heat");
+                    that.device.attributes.thermostatMode = 'heat';
+                    break;
+                case Characteristic.TargetHeatingCoolingState.AUTO:
+                    that.platform.api.runCommand(callback, that.deviceid, "auto");
+                    that.device.attributes.thermostatMode = 'auto';
+                    break;
+                case Characteristic.TargetHeatingCoolingState.OFF:
+                    that.platform.api.runCommand(callback, that.deviceid, "off");
+                    that.device.attributes.thermostatMode = 'off';
+                    break;
+            }
+        });
+        that.platform.addAttributeUsage("thermostatMode", this.deviceid, thisCharacteristic);
+        */
+    thisCharacteristic = this.getaddService(Service.HeaterCooler).getCharacteristic(Characteristic.CurrentTemperature)
+    thisCharacteristic.on('get', function (callback) {
+        if (that.platform.temperature_unit == 'C')
+            callback(null, parseFloat(that.device.attributes.temperature * 10) / 10);
+        else
+            callback(null, Math.round(((that.device.attributes.temperature - 32) / 1.8) * 10) / 10);
+    });
+    that.platform.addAttributeUsage("temperature", this.deviceid, thisCharacteristic);
+
+    thisCharacteristic = this.getaddService(Service.HeaterCooler).getCharacteristic(Characteristic.CoolingThresholdTemperature).setProps({ minValue: 18, maxValue: 30 });
+    thisCharacteristic.on('get', function (callback) { callback(null, parseInt(that.device.attributes.level)); });
+    thisCharacteristic.on('set', function (value, callback) { that.platform.api.runCommand(callback, that.deviceid, "setLevel", { value1: value }); });
+    that.platform.addAttributeUsage("level", this.deviceid, thisCharacteristic);
+
+}
+else if (device.commands.heater) {
+    this.deviceGroup = "thermostats";
+}
+else {	
+	
     this.deviceGroup = "thermostats";
 
     thisCharacteristic = this.getaddService(Service.Thermostat).getCharacteristic(Characteristic.CurrentHeatingCoolingState)
@@ -1698,6 +1808,7 @@ if (device.capabilities["Thermostat"] !== undefined) {
         that.device.attributes.coolingSetpoint = temp;
     });
     that.platform.addAttributeUsage("coolingSetpoint", this.deviceid, thisCharacteristic);
+}
 }
 this.loadData(device, this);
 }
